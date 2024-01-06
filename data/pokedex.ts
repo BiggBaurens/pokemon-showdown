b21d6f7ff2ba2ff81f9902817dace7d79920ec2c
BiggBaurens <154934746+BiggBaurens@users.noreply.github.com>
2024-01-06 17:05:11 +0100

    ds

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -11406,7 +11406,6 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
         weightkg: 48.5,
         color: "Yellow",
         eggGroups: ["Undiscovered"],
-		requiredAbility: "Resolute",
         battleOnly: "Keldeo",
     },
 	meloetta: {
