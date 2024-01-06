a9d7a2a7127868cf6d7e91fc565cdb63b4baa92f
BiggBaurens <154934746+BiggBaurens@users.noreply.github.com>
2024-01-06 16:15:06 +0100

    keldeo fix 1111

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -11381,7 +11381,6 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 	keldeo: {
 		num: 647,
 		name: "Keldeo",
-		baseForme: "Ordinary",
 		types: ["Water", "Fighting"],
 		gender: "N",
 		baseStats: {hp: 91, atk: 72, def: 90, spa: 129, spd: 90, spe: 108},
