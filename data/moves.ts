e2f0bfac26f0eefaf4dc23531fb0371da2f57a76
BiggBaurens <154934746+BiggBaurens@users.noreply.github.com>
2024-01-08 17:08:51 +0100

    Second time

--- data/moves.ts
+++ data/moves.ts
@@ -22404,39 +22404,26 @@ export const Moves: {[moveid: string]: MoveData} = {
 		num: 223,
 		accuracy: 90,
 		basePower: 80,
+		basePowerCallback(pokemon, target, move) {
+			if (pokemon.moveLastTurnResult === true) {
+					return Math.min(160, 80 + 16 * this.effectState.numConsecutive);
+			}
+			return move.basePower;
+		},
 		category: "Physical",
 		name: "Wombo Combo",
 		pp: 15,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1, punch: 1},
-		onTryHit(pokemon) {
-			pokemon.addVolatile('metronome');
-		},
 		condition: {
 			onStart(pokemon) {
-				this.effectState.lastMove = '';
 				this.effectState.numConsecutive = 0;
-			},
-			onTryMovePriority: -2,
-			onTryMove(pokemon, target, move) {
-				if (this.effectState.lastMove === move.id && pokemon.moveLastTurnResult) {
+				if (pokemon.moveLastTurnResult === true) {
 					this.effectState.numConsecutive++;
-				} else if (pokemon.volatiles['twoturnmove']) {
-					if (this.effectState.lastMove !== move.id) {
-						this.effectState.numConsecutive = 1;
-					} else {
-						this.effectState.numConsecutive++;
-					}
-				} else {
+				}
+				else {
 					this.effectState.numConsecutive = 0;
 				}
-				this.effectState.lastMove = move.id;
-			},
-			onModifyDamage(damage, source, target, move) {
-				const dmgMod = [4096, 4915, 5734, 6553, 7372, 8192];
-				const numConsecutive = this.effectState.numConsecutive > 5 ? 5 : this.effectState.numConsecutive;
-				this.debug(`Current Metronome boost: ${dmgMod[numConsecutive]}/4096`);
-				return this.chainModify([dmgMod[numConsecutive], 4096]);
 			},
 		},
 		secondary: null,
